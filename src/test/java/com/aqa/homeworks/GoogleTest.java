f3236ff06ca30b2720231f4f7296d30c67d9d660
Igor Stalnoy <7262736@gmail.com>
2024-04-08 22:38:24 +0300

    draft2

--- src/test/java/com/aqa/homeworks/GoogleTest.java
+++ src/test/java/com/aqa/homeworks/GoogleTest.java
@@ -4,15 +4,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class GoogleTest {
+    private List<WebDriver> webDriver = new ArrayList<>();
+    //    private WebDriver webDriver = new ChromeDriver();
+    public final static String CHROME_DRIVER_ROOT = "src/test/resources/chromedriver.exe";
     public final static String MAIN_URL = "https://www.onliner.by/";
     public final static String TVS_SECTION_LOCATOR = "//a[@href=\"https://catalog.onliner.by/tv\"]/span[@class=\"project-navigation__text\"]";
     public final static String FILTER_AWAITING_LOADER_LOCATOR = "//div[@class=\"catalog-interaction__state catalog-int" +
@@ -20,6 +28,10 @@ public class GoogleTest {
     public final static String FILTER_RESULT_LOCATOR = "//div[starts-with(@class,\"catalog-form__descripti" +
             "on catalog-form__description_primary catalog-form__description_base-additional catalog-form__description_font-weight_se" +
             "mibold catalog-form__description_condensed-other\")]/a[@href]";
+    public final static String SCROLL_ELEMENT_INTO_MIDDLE_SCRIPT = "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);"
+            + "var elementTop = arguments[0].getBoundingClientRect().top;"
+            + "window.scrollBy(0, elementTop-(viewPortHeight/2));";
+    public final static String OPEN_ALL_BRAND_FILTERS_LOCATOR = "//div[@class=\"catalog-form__row catalog-form__row_condensed-other\"][7]//div[@class=\"input-style__real\"]";
 
     @BeforeMethod
     public void driverInitialize() {
@@ -35,33 +47,38 @@ public class GoogleTest {
         WebDriverWait wait = new WebDriverWait(webDriver, 20);
         WebElement tvsSection = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(TVS_SECTION_LOCATOR)));
         tvsSection.click();
-        String filterLocator = "//div[contains(text(),\"" + filterName + "\")][@clas" +
-                "s=\"catalog-form__checkbox-sign\"]/../../div[@class=\"i-checkbox__faux\"]";
+        WebElement openAllBrandFilters = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(OPEN_ALL_BRAND_FILTERS_LOCATOR)));
+        ((JavascriptExecutor) webDriver).executeScript(SCROLL_ELEMENT_INTO_MIDDLE_SCRIPT, openAllBrandFilters);
+        openAllBrandFilters.click();
+        String filterLocator = "//div[contains(text(),\"" + filterName + "\")]/../../div[@class=\"i-checkbox__faux\"]";
         WebElement filter = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(filterLocator)));
-        String scrollElementIntoMiddle = "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);"
-                + "var elementTop = arguments[0].getBoundingClientRect().top;"
-                + "window.scrollBy(0, elementTop-(viewPortHeight/2));";
-        ((JavascriptExecutor) webDriver).executeScript(scrollElementIntoMiddle, filter);
         filter.click();
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(FILTER_AWAITING_LOADER_LOCATOR)));
         List<WebElement> filterResultList = webDriver.findElements(By.xpath(FILTER_RESULT_LOCATOR));
-        boolean isOnlyFilteredItemsDisplayed = filterResultList.stream()
-                .noneMatch(l -> l.getText().contains(filterName));
-        webDriver.quit();
+        Assert.assertFalse(filterResultList.stream()
+                .noneMatch(l -> l.getText().contains(filterName)));
+//        webDriver.quit();
     }
-//
-//    @DataProvider
-//    public
 
+    @DataProvider(name = "filterData")
+    public Object[][] filterData() {
+        System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_ROOT);
+        IntStream.range(0, 4)
+                .forEach(i -> webDriver.add(new ChromeDriver()));
+        return new Object[][]{
+                {webDriver.get(0), "Philips"},
+                {webDriver.get(1), "Sony"},
+                {webDriver.get(2), "BBK"},
+                {webDriver.get(3), "LG"}
+        };
+    }
 
-
-
-
-//    @AfterMethod
-//    public void cleanUp() {
-//        if (webDriver != null) {
-//            webDriver.quit();
-//        }
-//    }
-
+    @AfterTest()
+    public void cleanUp() {
+        webDriver.forEach(webDriver -> {
+            if (webDriver != null) {
+                webDriver.quit();
+            }
+        });
+    }
 }
